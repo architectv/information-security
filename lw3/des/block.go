c59448f60738b17928bd3045f4b2b7501b9546d3
Alexey Vasyukov <a.vasyukov1@yandex.ru>
2020-11-11 20:38:04 +0300

    [LW3] Passed

--- lw3/des/block.go
+++ lw3/des/block.go
@@ -1,17 +1,17 @@
 package des
 
-func initialPermutation(block64b uint64) uint64 {
+func blockPermutation(block64b uint64, perm [64]byte) uint64 {
 	var newBlock64b uint64
 	for i := 0; i < 64; i++ {
-		newBlock64b |= ((block64b >> (64 - _IP[i])) & 1) << (63 - i)
+		newBlock64b |= ((block64b >> (64 - perm[i])) & 1) << (63 - i)
 	}
 	return newBlock64b
 }
 
+func initialPermutation(block64b uint64) uint64 {
+	return blockPermutation(block64b, _IP)
+}
+
 func finalPermutation(block64b uint64) uint64 {
-	var newBlock64b uint64
-	for i := 0; i < 64; i++ {
-		newBlock64b |= ((block64b >> (64 - _FP[i])) & 1) << (63 - i)
-	}
-	return newBlock64b
+	return blockPermutation(block64b, _FP)
 }
